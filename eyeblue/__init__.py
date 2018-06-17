11eeac525fc5b9ff9755dc27fc9abc4f43c0abcd
blueye-info <rombocn@gmail.com>
2018-06-17 17:26:15 +0800

    update

--- eyeblue/__init__.py
+++ eyeblue/__init__.py
@@ -1,3 +1,6 @@
-from eyeblue.quotations import (get_depth,get_ticker,
+from eyeblue.quotations import (get_depth, get_ticker,
                                 get_k_data)
+
 from eyeblue.trade import (Util)
+
+from eyeblue.stream import (create_connection, WsClient)
