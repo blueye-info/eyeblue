d636a27a71812bf0400b90689e017e9c615fa08a
blueye-info <39900766+blueye-info@users.noreply.github.com>
2018-08-01 15:21:25 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name="eyeblue",
     packages=find_packages(),
-    version='1.0',
+    version='1.1.2',
     description="A utility for digital cash",
     author="banjuanshua",
     author_email='593474341@qq.com',
     url="https://www.blueye.info",
     download_url='',
     keywords='Digital Cash Data',
-    install_requires=[
-        'pandas',
-        'numpy',
-    ]
+
 )
