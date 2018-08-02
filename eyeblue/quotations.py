d146a4241be9005359eb7e7202345a2b1a3e9f94
blueye-info <39900766+blueye-info@users.noreply.github.com>
2018-08-02 23:24:54 +0800

    Update quotations.py

--- eyeblue/quotations.py
+++ eyeblue/quotations.py
@@ -7,13 +7,13 @@ from urllib import request
 def get_k_data(symbol, k_type, exchange='bitfinex',
                since=0, size=100):
 
-    if exchange not in cf.all_exchage:
+    '''if exchange not in cf.all_exchage:
         raise NameError('exchange not exits')
     else:
         if symbol not in cf.all_ex_sy[exchange]:
             raise NameError('symbol not eixts')
     if k_type not in cf.all_k_type:
-        raise NameError('type not eixts')
+        raise NameError('type not eixts')'''
 
     re_url = 'http://kline.blueye.info/bitquote/v1/kline?' \
              'exchange={}&symbol={}&since={}&size={}&' \
